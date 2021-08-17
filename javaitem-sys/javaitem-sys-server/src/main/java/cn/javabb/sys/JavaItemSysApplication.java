023dd47bf5af520e32644ecd8497999e0d80b700
imqinbao <imqinbao@163.com>
2021-08-18 00:48:35 +0800

    init

--- javaitem-sys/javaitem-sys-server/src/main/java/cn/javabb/sys/JavaItemSysApplication.java
+++ javaitem-sys/javaitem-sys-server/src/main/java/cn/javabb/sys/JavaItemSysApplication.java
@@ -4,6 +4,7 @@ import cn.javabb.security.annotation.EnableCustomConfig;
 import cn.javabb.security.annotation.EnableCustomFeignClients;
 import cn.javabb.swagger.annotation.EnableCustomSwagger;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 
 /**
@@ -14,7 +15,7 @@ import org.springframework.cloud.client.SpringCloudApplication;
 @EnableCustomConfig
 @EnableCustomSwagger
 @EnableCustomFeignClients
-@SpringCloudApplication
+@SpringBootApplication
 public class JavaItemSysApplication {
     public static void main(String[] args) {
         SpringApplication.run(JavaItemSysApplication.class, args);
